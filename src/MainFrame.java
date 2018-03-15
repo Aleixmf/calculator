5a3e592b30a60ac36e2a986a3b65914fb8bf6ae4
alu53729679t <alu53729679t@192.168.201.108>
2018-03-15 20:27:56 +0100

    solved cosecutive calls to operators

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -368,35 +368,47 @@ public class MainFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_btnCActionPerformed
 
     private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+         if (!erase) {
         erase = true;
         calculateResult();
         displayResult();
         operator = OperatorType.ADD;
+         }
     }//GEN-LAST:event_btnAddActionPerformed
 
     private void btnSubtractActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubtractActionPerformed
+         if (!erase) {
         erase = true;
         calculateResult();
         displayResult();
         operator = OperatorType.SUBTRACT;
+         }
     }//GEN-LAST:event_btnSubtractActionPerformed
 
     private void btnMultiplyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMultiplyActionPerformed
+       if (!erase) {
         erase = true;
         calculateResult();
         displayResult();
         operator = OperatorType.MULTIPLY;
+       }
     }//GEN-LAST:event_btnMultiplyActionPerformed
 
     private void btnDivideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDivideActionPerformed
+         if (!erase) {
         erase = true;
         calculateResult();
         displayResult();
         operator = OperatorType.DIVIDE;
+         }
     }//GEN-LAST:event_btnDivideActionPerformed
 
     private void btnEqualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEqualActionPerformed
-        // TODO add your handling code here:
+       if (!erase) {
+        erase = true;
+        calculateResult();
+        displayResult();
+        }
     }//GEN-LAST:event_btnEqualActionPerformed
 
     private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
